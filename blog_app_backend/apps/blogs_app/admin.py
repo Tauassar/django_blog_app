5c65040e973cb6fff27d429986f8eb15646dc410
Tauassar <tauassar.tatiyev@nu.edu.kz>
2022-05-15 18:30:54 +0600

    created models added to admin page

--- blog_app_backend/apps/blogs_app/admin.py
+++ blog_app_backend/apps/blogs_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from apps.blogs_app.models import BlogEntity
+from apps.blogs_app.models import BlogEntity, ReadBlogs
 
 admin.site.register(BlogEntity)
+admin.site.register(ReadBlogs)
